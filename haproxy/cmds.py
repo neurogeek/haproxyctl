9bee771e780bd34cc7439f9eb0233c5c3f21d0ca
Jesus Rivero <jesus.riveroa@gmail.com>
2013-07-12 20:59:40 -0400

    Code reflow and removal of additional command line

--- haproxy/cmds.py
+++ haproxy/cmds.py
@@ -107,8 +107,6 @@ class baseStat(Cmd):
             return dict((a, i) for i, a in enumerate(mobj.groupdict()['columns'].split(',')))
         raise Exception("Could not parse columns from HAProxy output")
 
-## pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,comp_in,comp_out,comp_byp,comp_rsp,
-
 class listServers(baseStat):
     """Show servers in the given backend"""
 
